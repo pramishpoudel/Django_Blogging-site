2e569ebc1690ffd49369aa7faa51d299c5c7a245
pramish poudel <pramishpoudel502@gmail.com>
2025-12-20 01:37:30 +0545

    dynamic blog content dispalyed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import Category,Blog
 
 # Create your views here.
 def home_blogs(request):
-    return render(request, 'home-blogs.html')
+    categories = Category.objects.all()
+    featured_blogs = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False,status='Publish')
+    context={ 
+            'categories': categories,
+            'featured_blogs': featured_blogs,
+            'posts': posts,
+             }
+    return render(request, 'home-blogs.html',context)
+
+
